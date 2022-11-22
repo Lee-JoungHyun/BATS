7906aa1ce1bed87956390b7bdb6afd2301e057f3
JoungHyunLee <98592001+DoublyH@users.noreply.github.com>
2022-11-22 13:27:56 +0900

    Chart_v1
    차트 그리기만 성공

--- Bats/app/src/main/java/com/cookandroid/bats/PersonalMain.java
+++ Bats/app/src/main/java/com/cookandroid/bats/PersonalMain.java
@@ -1,5 +1,7 @@
 package com.cookandroid.bats;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,37 +12,20 @@ import com.github.mikephil.charting.charts.CandleStickChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CandleData;
 import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.interfaces.datasets.ICandleDataSet;
 
 import java.util.ArrayList;
 
 public class PersonalMain extends AppCompatActivity {
 
-    //-- 차트 그리기 위한 하나의 캔들 클래스 --//
-    class Candle {
-        Candle(float createAt, float open, float close, float shadowHigh, float shadowLow) {
-            this.createAt = createAt;
-            this.open = open;
-            this.close = close;
-            this.shadowHigh = shadowHigh;
-            this.shadowLow = shadowLow;
-            if(open > close)
-                this.red = false;
-            else
-                this.red = true;
-        }
-        float createAt;
-        float open;
-        float close;
-        float shadowHigh;
-        float shadowLow;
-        boolean red;
-    }
     CandleStickChart candleStickChart;
     Button onBtn;
     TextView state;
     ArrayList candleList = new ArrayList();
+    ArrayList yValsCandleStick = new ArrayList();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_main);
@@ -49,11 +34,11 @@ public class PersonalMain extends AppCompatActivity {
         state = (TextView) findViewById(R.id.stateTxt);
 
         ///////////////////// 필요 없을 부분(캔들 임시 생성) ////////////////////////
-        Candle tmp = new Candle(0, 200F, 300F, 500F, 100F);
-        Candle tmp2 = new Candle(1, 250F, 400F, 450F, 200F);
-        Candle tmp3 = new Candle(2, 270F, 100F, 3500F, 50F);
-        Candle tmp4 = new Candle(3, 100F, 150F, 200F, 70F);
-        Candle tmp5 = new Candle(4, 170F, 80F, 220F, 30F);
+        CandleEntry tmp = new CandleEntry (0, 500F, 100F, 200F, 300F);
+        CandleEntry tmp2 = new CandleEntry (1, 450F, 200F,250F, 400F);
+        CandleEntry tmp3 = new CandleEntry (2, 350F, 50F,270F, 100F);
+        CandleEntry tmp4 = new CandleEntry(3, 200F, 70F,100F, 150F);
+        CandleEntry tmp5 = new CandleEntry(4, 220F, 30F, 170F, 80F);
 
         candleList.add(tmp);
         candleList.add(tmp2);
@@ -61,31 +46,20 @@ public class PersonalMain extends AppCompatActivity {
         candleList.add(tmp4);
         candleList.add(tmp5);
         ///////////////////// 필요 없을 부분(캔들 임시 생성) ////////////////////////
-        candleStickChart.setHighlightPerDragEnabled(true);
 
-        candleStickChart.setDrawBorders(true);
-        candleStickChart.setBorderColor(getResources().getColor(R.color.black));
-        YAxis yAxis = candleStickChart.getAxisLeft();
-        YAxis rightAxis = candleStickChart.getAxisRight();
-        yAxis.setDrawGridLines(false);
-        rightAxis.setDrawGridLines(false);
-        candleStickChart.requestDisallowInterceptTouchEvent(true);
-        XAxis xAxis = candleStickChart.getXAxis();
-        xAxis.setDrawGridLines(false);// disable x axis grid lines
-        xAxis.setDrawLabels(false);
-        rightAxis.setTextColor(android.R.color.white);
-        yAxis.setDrawLabels(false);
-        xAxis.setGranularity(1f);
-        xAxis.setGranularityEnabled(true);
-        xAxis.setAvoidFirstLastClipping(true);
-        Legend l = candleStickChart.getLegend();
-        l.setEnabled(false);
+        CandleDataSet candleDataSet = new CandleDataSet(candleList, "BTC markets");
+        //candleDataSet.setColor();
+        candleDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        candleDataSet.setShadowColor(Color.BLACK);
+        candleDataSet.setShadowWidth(2f);
+        candleDataSet.setDecreasingColor(Color.BLUE);
+        candleDataSet.setDecreasingPaintStyle(Paint.Style.FILL);
+        candleDataSet.setIncreasingPaintStyle(Paint.Style.FILL);
+        candleDataSet.setIncreasingColor(Color.RED);
+        candleDataSet.setNeutralColor(Color.BLACK);
 
-
-
-        for (int i=0; i<candleList.size(); i++) {
-
-        }
-
+        CandleData candleData = new CandleData(candleDataSet);
+        candleStickChart.setData(candleData);
+        candleStickChart.invalidate();
     }
 }
