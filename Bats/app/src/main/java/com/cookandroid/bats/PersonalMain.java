e3269d31ae1011f37e22b0693c272866a8d9125d
JoungHyunLee <98592001+DoublyH@users.noreply.github.com>
2022-11-30 00:55:10 +0900

    realtimeChart
    실시간 차트 그리기 성공

--- Bats/app/src/main/java/com/cookandroid/bats/PersonalMain.java
+++ Bats/app/src/main/java/com/cookandroid/bats/PersonalMain.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.mikephil.charting.charts.CandleStickChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
@@ -19,14 +26,89 @@ import com.github.mikephil.charting.data.CandleDataSet;
 import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.interfaces.datasets.ICandleDataSet;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PersonalMain extends AppCompatActivity {
 
     CandleStickChart candleStickChart;
     Button onBtn, showlog;
     TextView state;
-    ArrayList candleList = new ArrayList();
+    ArrayList<CandleEntry> candleList = new ArrayList();
+    private RequestQueue rQ;
+
+    Timer timer = new Timer();
+    TimerTask TT = new TimerTask() {
+        @Override
+        public void run() {
+            drawChart();
+        }
+    };
+    private void drawChart() {
+        candleList.clear();
+        String url = "https://api.upbit.com/v1/candles/minutes/1?market=KRW-BTC&count=15";
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        getTickerData(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Main", "onErrorResponse:" + error.getMessage());
+                    }
+                });
+        request.setShouldCache(false);
+        rQ.add(request);
+    }
+    private void getTickerData(String data) {
+        try {
+            JSONArray jsonArray = new JSONArray(data);
+
+            for(int i=0; i<jsonArray.length(); i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                int open = jsonObject.getInt("opening_price");
+                int close = jsonObject.getInt("trade_price");
+                int sHigh = jsonObject.getInt("high_price");
+                int sLow = jsonObject.getInt("low_price");
+
+                CandleEntry tmp = new CandleEntry((float)(jsonArray.length()-1-i), (float)sHigh, (float)sLow, (float)open, (float)close);
+                candleList.add(tmp);
+            }
+            ArrayList<CandleEntry> tmp = new ArrayList();
+            for(int i=candleList.size()-1; i>=0; i--) {
+                tmp.add(candleList.get(i));
+            }
+
+            CandleDataSet candleDataSet = new CandleDataSet(tmp, "BTC markets");
+            //candleDataSet.setColor();
+            candleDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+            candleDataSet.setShadowColor(Color.BLACK);
+            candleDataSet.setShadowWidth(2f);
+            candleDataSet.setDecreasingColor(Color.BLUE);
+            candleDataSet.setDecreasingPaintStyle(Paint.Style.FILL);
+            candleDataSet.setIncreasingPaintStyle(Paint.Style.FILL);
+            candleDataSet.setIncreasingColor(Color.RED);
+            candleDataSet.setNeutralColor(Color.BLACK);
+
+            CandleData candleData = new CandleData(candleDataSet);
+            candleStickChart.setData(candleData);
+            candleStickChart.invalidate();
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +118,11 @@ public class PersonalMain extends AppCompatActivity {
         state = (TextView) findViewById(R.id.stateTxt);
         showlog = (Button) findViewById(R.id.btn_log);
 
+        if (rQ == null) {
+            //리퀘스트큐 생성 (MainActivit가 메모리에서 만들어질 때 같이 생성이 될것이다.
+            rQ = Volley.newRequestQueue(getApplicationContext());
+        }
+
         showlog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,33 +130,33 @@ public class PersonalMain extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-                ///////////////////// 필요 없을 부분(캔들 임시 생성) ////////////////////////
-                CandleEntry tmp = new CandleEntry(0, 500F, 100F, 200F, 300F);
-        CandleEntry tmp2 = new CandleEntry (1, 450F, 200F,250F, 400F);
-        CandleEntry tmp3 = new CandleEntry (2, 350F, 50F,270F, 100F);
-        CandleEntry tmp4 = new CandleEntry(3, 200F, 70F,100F, 150F);
-        CandleEntry tmp5 = new CandleEntry(4, 220F, 30F, 170F, 80F);
+        drawChart();
+        (new Thread(new Runnable()
+        {
 
-        candleList.add(tmp);
-        candleList.add(tmp2);
-        candleList.add(tmp3);
-        candleList.add(tmp4);
-        candleList.add(tmp5);
-        ///////////////////// 필요 없을 부분(캔들 임시 생성) ////////////////////////
+            @Override
+            public void run()
+            {
+                while (!Thread.interrupted())
+                    try
+                    {
+                        Thread.sleep(60000);
+                        runOnUiThread(new Runnable() // start actions in UI thread
+                        {
 
-        CandleDataSet candleDataSet = new CandleDataSet(candleList, "BTC markets");
-        //candleDataSet.setColor();
-        candleDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-        candleDataSet.setShadowColor(Color.BLACK);
-        candleDataSet.setShadowWidth(2f);
-        candleDataSet.setDecreasingColor(Color.BLUE);
-        candleDataSet.setDecreasingPaintStyle(Paint.Style.FILL);
-        candleDataSet.setIncreasingPaintStyle(Paint.Style.FILL);
-        candleDataSet.setIncreasingColor(Color.RED);
-        candleDataSet.setNeutralColor(Color.BLACK);
+                            @Override
+                            public void run()
+                            {
+                                drawChart();
+                            }
+                        });
+                    }
+                    catch (InterruptedException e)
+                    {
+                        // ooops
+                    }
+            }
+        })).start();
+    } //Timer 실행
 
-        CandleData candleData = new CandleData(candleDataSet);
-        candleStickChart.setData(candleData);
-        candleStickChart.invalidate();
-    }
 }
